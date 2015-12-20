355d469187e8df6207c0202fbddbb3cf8dc57382
adityasan92 <adityasan92@gmail.com>
2015-12-19 19:58:10 -0500

    Added the JSON functionality

--- scrape.py
+++ scrape.py
@@ -1,6 +1,6 @@
 import urllib
 import re
-
+import json
 htmltext= urllib.urlopen("https://www.google.ca/finance/getprices?q=AAPL&x=NASD&i=120&p=25m&f=c&df=cpct&auto=1&ts=1450510581558&ei=wQh1Vrm9HdeVjAGc56nwBg").read()
 
 print htmltext.split()[len(htmltext.split()) -1]
@@ -10,3 +10,9 @@ print htmltext.split()[len(htmltext.split()) -1]
 #results = re.findall(pattern,htmltext)
 
 #print results
+
+htmlJson = urllib.urlopen('http://www.bloomberg.com/markets/watchlist/recent-ticker/AAPL:AR')
+
+data = json.load(htmlJson)
+
+print data["last_price"]
